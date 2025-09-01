23133456802fdf57da633ebc6626ec88480e3800
ogcelio <joaocelioaraujosander@gmail.com>
2025-08-31 21:35:56 -0300

    readicionando linked_list

--- alg/merge/linked_list_string.cpp
+++ alg/merge/linked_list_string.cpp
@@ -2,86 +2,73 @@
 #include "linked_list_string.h"
 #include <cstdlib>
 #include <cstring>
+#include <cstdio>
+#include <string>
 #include <iostream>
 
 #define MAX_NUMERO_DE_CARACTERES 64
 struct No {
-  //char *name;
-  char name[MAX_NUMERO_DE_CARACTERES];
-  /*
-   opcional: convencao de '\0' no final
-  */
-  No *next;
+	// char *name;
+	
+	 char name[MAX_NUMERO_DE_CARACTERES];
+	// opcional: convencao de '\0' no final
+	 
+	No *next;
 };
 
 typedef No *Lista;
-
-// ---------------------------------------------------------------
-// mesma lista, mas com ponteiro pra char
+//----------------------------------
 struct No2 {
-  char *name;
-  //char name[MAX_NUMERO_DE_CARACTERES];
-  /*
-   opcional: convencao de '\0' no final
-  */
-  No *next;
+	char *name;
+
+	No2 *next;
 };
 
 typedef No2 *Lista2;
 
 void 
-playground_lista()
-{
-  std::cout << "teste" << std::endl;
-  Lista l;
+playground_lista2(){
+	Lista2 l;
 
-  // cria um novo no
-  
-  l = (No *) malloc(sizeof(No));
+	l = (No2 *) malloc(sizeof(No2));
+	l -> name = 0;
+    
+    // usando \0 (necessario alocar memoria para o caractere)
+    l->name = (char *) malloc(sizeof(1));
+	l->name[0] = '\0';
+    printf("%s", l->name);
+    printf("print vazio realizado\n");
 
-  // no caso da implementacao estatica
-  l->name[0] = '\0';
-  
-  // ou no caso do char * :
-  //l->name = "";
-  //l->name = 0;
+    // ou
+    // l->name = "";
+	// printf("%s", l->name);
+    // printf("print vazio realizado\n");
 
-  std::cout << std::string(l->name) << std::endl;
-
-  std::cout << "teste" << std::endl;
-  // ou
-  printf("%s", l->name); // assume que nome contem '\0'
-  
-  strncpy(l->name, "joao", MAX_NUMERO_DE_CARACTERES);
-  
-  printf("%s", l->name); // assume que nome contem '\0'
+	l -> name = "joao\n";
+	printf("%s", l->name);	
 }
 
-void 
-playground_lista2()
+void
+playground_lista()
 {
-  // Exercicio1: prencher com lista 2
-  std::cout << "teste" << std::endl;
-  Lista l;
+	Lista l;
 
-  // cria um novo no
-  
-  l = (No *) malloc(sizeof(No));
+	// cria um novo no
+	
+	l = (No *) malloc(sizeof(No));
+	
+	//no cado da implementacao estatica
+	l->name[0] = '\0';
 
-  // no caso da implementacao estatica
-  l->name[0] = '\0';
-  
-  // ou no caso do char * :
-  //l->name = "";
-  //l->name = 0;
+	//ou no caso do char* :
+	// l->name = "";
+	// l->name = 0;
 
-  std::cout << std::string(l->name) << std::endl;
 
-  std::cout << "teste" << std::endl;
-  // ou
-  printf("%s", l->name); // assume que nome contem '\0'
-  
-  strncpy(l->name, "joao", MAX_NUMERO_DE_CARACTERES);
-  
-  printf("%s", l->name); // assume que nome contem '\0'
+	std::cout << std::string(l->name) <<std::endl;
+	// ou
+	printf("%s", l->name); //assume que nome contem '\0'
+	strncpy(l->name, "joao\n", MAX_NUMERO_DE_CARACTERES);
+	printf("%s", l->name); //assume que nome contem '\0'
+
 }
