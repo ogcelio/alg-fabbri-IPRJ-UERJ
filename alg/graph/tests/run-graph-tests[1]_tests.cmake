6a44ab94f282ba3a98e8a84491e07377f6a85dc8
ogcelio <joaocelioaraujosander@gmail.com>
2025-11-02 16:15:16 -0300

    graph tests

--- alg/graph/tests/run-graph-tests[1]_tests.cmake
+++ alg/graph/tests/run-graph-tests[1]_tests.cmake
@@ -1,3 +1,3 @@
-add_test([=[GraphTest.BasicAssertions]=]  /home/pescatore/.a/alg-fabbri-IPRJ-UERJ/alg/graph/tests/run-graph-tests [==[--gtest_filter=GraphTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[GraphTest.BasicAssertions]=]  PROPERTIES WORKING_DIRECTORY /home/pescatore/.a/alg-fabbri-IPRJ-UERJ/alg/graph/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+add_test([=[GraphTest.BasicAssertions]=]  /home/synthyaga/.a/alg-fabbri-IPRJ-UERJ/alg/graph/tests/run-graph-tests [==[--gtest_filter=GraphTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[GraphTest.BasicAssertions]=]  PROPERTIES WORKING_DIRECTORY /home/synthyaga/.a/alg-fabbri-IPRJ-UERJ/alg/graph/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
 set(  run-graph-tests_TESTS GraphTest.BasicAssertions)
